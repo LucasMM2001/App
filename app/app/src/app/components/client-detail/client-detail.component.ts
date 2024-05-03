7dbed0fb2a5f686ce7eb1aa6b09c641def570dea
Lucas Miady Miquelin <129232064+LucasMM2001@users.noreply.github.com>
2024-05-02 22:24:10 -0300

    Foi acrescentado a funcionalidade
    Foi acrescentado a funcionalidade inserir,remover,alterar tabela

--- app/app/src/app/components/client-detail/client-detail.component.ts
+++ app/app/src/app/components/client-detail/client-detail.component.ts
@@ -1,16 +1,24 @@
+import { ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
+import { Cliente } from './../../interfaces/Cliente';
+import { ClienteService } from './../../services/cliente.service';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+
+
 
 @Component({
   selector: 'app-client-detail',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,],
   templateUrl: './client-detail.component.html',
   styleUrl: './client-detail.component.css'
 })
 export class ClientDetailComponent {
 
-  constructor(private route:ActivatedRoute){
+  cliente?:Cliente;
+  clienteForm: FormGroup = new FormGroup({})
+  constructor(private route:ActivatedRoute,private ClienteService:ClienteService,private formBuilder: FormBuilder){
 
     this.getClientById()
 
@@ -19,9 +27,31 @@ export class ClientDetailComponent {
   id?:string;
   getClientById(){
     this.id = this.route.snapshot.paramMap.get('id')??'';
-    alert(this.id);
+    this.ClienteService.getById(this.id).subscribe((clienteResponse) => this.cliente = clienteResponse);
+    
 
+    this.clienteForm = this.formBuilder.group({
+
+      nome: [this.cliente?.nome],
+      telefone: [this.cliente?.telefone],
+      id:[this.cliente?.id]
+    })
+    
 
   }
 
+  update():void{
+
+    if(this.clienteForm.valid){
+      const clienteNovo:Cliente = {
+        nome: this.clienteForm.value.nome,
+        telefone: this.clienteForm.value.telefone,
+        id: this.clienteForm.value.id
+      }
+      this.ClienteService.atualizar(clienteNovo).subscribe()
+      alert('Alterado com sucesso')
+  }
+
 }
+}
+
--- app/app/src/app/services/cliente.service.ts
+++ app/app/src/app/services/cliente.service.ts
@@ -3,11 +3,15 @@ import { ObservableLike } from './../../../node_modules/dot-prop/node_modules/ty
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { application } from 'express';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  add(clienteAdd: Cliente) {
+    throw new Error('Method not implemented.');
+  }
   private clientesUrl = "http://localhost:3000/clientes";
   constructor(private http: HttpClient) { }
 
@@ -21,16 +25,40 @@ export class ClienteService {
     //return this.clientes;
   }
 
-  remover(id:string){
-    const cliente = this.clientes.find(c => c.id == id);
+  getById(id:string) : Observable<Cliente>
+  {
+     return this.http.get(`${this.clientesUrl}/${id}`) as Observable<Cliente>
+  }
 
-    if(cliente){
-       const index = this.clientes.indexOf(cliente);
-       this.clientes.splice(index,1);
+  remover(id:string){
+  //   const cliente = this.clientes.find(c => c.id == id);
+
+  //   if(cliente){
+  //      const index = this.clientes.indexOf(cliente);
+  //      this.clientes.splice(index,1);
+  //   }
+  return this.http.delete(`${this.clientesUrl}/${id}`)
+  }
+
+
+   httpHeader = {
+    headers:{
+      "Content-Type":"application/json"
     }
+  };
+
+
+  atualizar(cliente:Cliente){
+  
+ return this.http.put(`${this.clientesUrl}/${cliente.id}`,cliente,this.httpHeader)
+
   }
 
   adicionar(cliente:Cliente){
-    this.clientes.push(cliente);
+  
+
+    return this.http.post(this.clientesUrl,cliente,this.httpHeader)
+
+    // this.clientes.push(cliente);
   }
 }
